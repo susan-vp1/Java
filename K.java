88e1a6b6d15eebd651170581f26a1250ffcf67ee
susan-vp1 <115590069+susan-vp1@users.noreply.github.com>
2022-10-11 23:01:53 -0400

    Add files via upload

---
+++ K.java
@@ -0,0 +1,11 @@
+class K 
+{
+	public static void main(String[] args) 
+	{
+		System.out.print(9);
+		System.out.print(8);
+		System.out.print(7);
+		System.out.print(6);
+		System.out.print(5);
+	}
+}
