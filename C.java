34d3df54bcdc69291eedcb3d9143a0b808ab9916
susan-vp1 <115590069+susan-vp1@users.noreply.github.com>
2025-03-15 22:17:46 -0400

    Update and rename C.java to FactorialCalculation.java

--- C.java
+++
@@ -1,7 +0,0 @@
-class C 
-{
-	public static void main(String[] args) 
-	{
-		System.out.println("Hello World!");
-	}
-}
---
+++ FactorialCalculation.java
@@ -0,0 +1,12 @@
+package com.java.practice;
+
+public class FactorialCalculation {
+	public static void main(String args[]) {
+		int number = 5;
+		int factorial = 1;
+		for(int i = 1; i <= number; i++) {
+			factorial = factorial * i;
+		}
+		System.out.println("Factorial of " + number + "is: " + factorial);
+	}
+}
